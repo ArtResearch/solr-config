088765ff54e3078fb2e83e348e6be1a7c3c09b76
Nikos <nikosxez@gmail.com>
2022-06-06 15:16:44 +0300

    fixing user

--- fix-folder-permissions.sh
+++ fix-folder-permissions.sh
@@ -14,7 +14,7 @@ mkdir -p ${SERVER_CONF}
 echo "Changing ownership of the solr folder: ${SERVER_CONF}"
 
 # change ownership of the runtime-data folder to uid/guid that is used inside SOLR docker container
-sudo chown -R 8983:0 ${SERVER_CONF}
+sudo chown -R 8983:8983 ${SERVER_CONF}
 
 # make sure that folder has correct write permissions and new files that are created inside the folder inherit ownership
 sudo chmod -R g+ws ${SERVER_CONF}
